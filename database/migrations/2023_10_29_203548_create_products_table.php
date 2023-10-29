fdd70c08c475577b37be923a3b8f52ad9cd9edfb
testxxxxxxxxx <pppppmarcin@gmail.com>
2023-10-29 21:02:59 +0000

    created ProductsController and CategoriesController and Product model and Category model

---
+++ database/migrations/2023_10_29_203548_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->float('price')->nullable(false);
+            $table->float('weight')->default(0);
+            $table->bigInteger('category_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+
+            $table->foreign('category_id')->references('id')->on('categories');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
