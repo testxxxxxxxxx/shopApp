24a8f06f808dc6ce5ef2d356f8bdb29eaec25612
testxxxxxxxxx <pppppmarcin@gmail.com>
2023-12-03 20:47:32 +0000

    created methods for ProductService

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 
 namespace App\Services;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+
 class ProductService
 {
     protected int $id;
@@ -12,6 +15,25 @@ class ProductService
     {
         $this->id = $i;
         
+    }
+
+    public function getName(int $id): string
+    {
+        $name = Product::query()->find($id)->get('name');
+
+        return $name[0]['name'];
+    }
+    public function getPrice(int $id): float
+    {
+        $price = Product::query()->find($id)->get('price');
+
+        return $price[0]['price'];
+    }
+    public function getWeight(int $id): float
+    {
+        $weight = Product::query()->find($id)->get('weight');
+
+        return $weight[0]['price'];
     }
 
 }
