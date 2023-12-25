9baea1e7c13bfc409c0fdaa47b8457615f23a1fd
testxxxxxxxxx <pppppmarcin@gmail.com>
2023-12-25 21:34:36 +0000

    Created ProductsRequest

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\ProductService;
+use App\Http\Requests\ProductsRequest;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Support\Facades\Auth;
 
 class ProductsController extends Controller
 {
-    
+    public function __construct(private ProductService $productService)
+    {
+        $this->productService = $productService;
+
+    }
+
+    public function index(ProductsRequest $productsRequest): Factory
+    {
+        if(Auth::check())
+        {
+        
+            return view('index', []);
+        }
+
+        return redirect()->back();
+    }
+    public function show(ProductsRequest $productsRequest): Factory
+    {
+        if(Auth::check())
+        {
+
+            return view('index', []);
+        }
+
+        return redirect()->back();
+    }
+
 }
